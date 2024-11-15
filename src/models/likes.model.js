1da2dc380286d01fcc7b2160b8416295c9006379
Sourav0010 <mohantysourav656@gmail.com>
2024-11-15 15:14:55 +0530

    implement like functionality for videos, comments, and tweets with validation and response handling

--- src/models/likes.model.js
+++ src/models/likes.model.js
@@ -4,13 +4,11 @@ const likeSchema = new Schema(
     {
         comment: {
             type: Schema.Types.ObjectId,
-            required: true,
             ref: 'Comment',
         },
         video: {
             type: Schema.Types.ObjectId,
             ref: 'Video',
-            required: true,
         },
         likedBy: {
             type: Schema.Types.ObjectId,
@@ -20,7 +18,6 @@ const likeSchema = new Schema(
         tweet: {
             type: Schema.Types.ObjectId,
             ref: 'Tweet',
-            required: true,
         },
     },
     { timestamps: true }
